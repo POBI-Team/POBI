89e6e22d201e5f616e031d00a0ebc42b68004fe8
Siwon-L <111dltldnjs@gmail.com>
2025-02-15 13:15:29 +0900

    chore: create project

---
+++ Pobi/PobiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PobiApp.swift
+//  Pobi
+//
+//  Created by 이시원 on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct PobiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
