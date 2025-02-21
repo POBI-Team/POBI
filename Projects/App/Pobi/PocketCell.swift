d28f5e468f43bb78a3d10f30dd6cddadc8ac5e56
Siwon-L <111dltldnjs@gmail.com>
2025-02-21 11:12:17 +0900

    feat: PocketCell

---
+++ Projects/App/Pobi/PocketCell.swift
@@ -0,0 +1,71 @@
+//
+//  PocketCell.swift
+//  Pobi
+//
+//  Created by 이시원 on 2/19/25.
+//
+
+import SwiftUI
+
+import PBDesignSystem
+
+struct PocketCell: View {
+  private let listColor: PBListColor.Type
+  
+  init(listColor: PBListColor.Type) {
+    self.listColor = listColor
+  }
+  
+  var body: some View {
+    VStack(alignment: .leading) {
+      Spacer()
+        .frame(height: 16)
+      Circle()
+        .foregroundStyle(listColor._01.color)
+        .frame(width: 32, height: 32)
+        .padding(.leading, 16)
+      Spacer()
+      VStack(alignment: .leading, spacing: 5) {
+        Text("Hello, world!")
+          .font(PBFonts.body._1.font)
+          .foregroundStyle(PBColors.navy._900.color)
+        Text("Hello, world!")
+          .font(PBFonts.caption._2.font)
+          .foregroundStyle(PBColors.navy._400.color)
+      }
+      .padding(.horizontal, 16)
+      Spacer()
+      HStack {
+        Text("Hello, world!")
+          .font(PBFonts.caption._2.font)
+          .foregroundStyle(PBColors.navy._900.color)
+        Spacer()
+        PBImages.next.image
+      }
+      .padding(.leading, 16)
+      .padding(.trailing, 12)
+      .frame(height: 40)
+      .background(listColor._02.color)
+      .clipShape(RoundedRectangle(cornerRadius: 20))
+    }
+    .background(listColor._03.color)
+    .clipShape(RoundedRectangle(cornerRadius: 20))
+    .aspectRatio(1, contentMode: .fit)
+    .overlay(alignment: .top) {
+      HStack {
+        Spacer()
+        Button {
+          
+        } label: {
+          PBImages.manu_16.image
+        }
+      }
+      .padding(.top, 20)
+      .padding(.trailing, 12)
+    }
+  }
+}
+
+#Preview {
+  PocketCell(listColor: PBColors.list.yellow.self)
+}
--- Projects/PBDesignSystem/PBDesignSystem/Sources/PBImages.swift
+++ Projects/PBDesignSystem/PBDesignSystem/Sources/PBImages.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 public struct PBImages {
   public static var plus: UIImage { UIImage(named: "plus", in: .module, with: nil)! }
+  public static var next: UIImage { UIImage(named: "next", in: .module, with: nil)! }
+  public static var manu_16: UIImage { UIImage(named: "manu_16", in: .module, with: nil)! }
 }
 
 public extension UIImage {
