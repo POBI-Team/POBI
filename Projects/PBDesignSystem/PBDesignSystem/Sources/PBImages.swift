264acef6eac4fb44584f9f91d492783935fd1874
Siwon-L <111dltldnjs@gmail.com>
2025-02-21 21:52:59 +0900

    add: imageSet 추가
    - clock, deactivateCheckBox, uncheckedCheckBox, checkedCheckBox

--- Projects/PBDesignSystem/PBDesignSystem/Sources/PBImages.swift
+++ Projects/PBDesignSystem/PBDesignSystem/Sources/PBImages.swift
@@ -11,6 +11,10 @@ public struct PBImages {
   public static var plus: UIImage { UIImage(named: "plus", in: .module, with: nil)! }
   public static var next: UIImage { UIImage(named: "next", in: .module, with: nil)! }
   public static var manu_16: UIImage { UIImage(named: "manu_16", in: .module, with: nil)! }
+  public static var clock: UIImage { UIImage(named: "clock", in: .module, with: nil)! }
+  public static var deactivateCheckBox: UIImage { UIImage(named: "deactivate_checkBox", in: .module, with: nil)! }
+  public static var uncheckedCheckBox: UIImage { UIImage(named: "unchecked_checkBox", in: .module, with: nil)! }
+  public static var checkedCheckBox: UIImage { UIImage(named: "checked_checkBox", in: .module, with: nil)! }
 }
 
 public extension UIImage {
