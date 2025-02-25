f9859959fef0740ac83e7e763e82613b466fbf12
Siwon-L <111dltldnjs@gmail.com>
2025-02-25 19:17:15 +0900

    create: PBStorageInterface target

---
+++ Projects/PBStorage/PBStorageInterface/PBStorageInterface.h
@@ -0,0 +1,18 @@
+//
+//  PBStorageInterface.h
+//  PBStorageInterface
+//
+//  Created by 이시원 on 2/25/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PBStorageInterface.
+FOUNDATION_EXPORT double PBStorageInterfaceVersionNumber;
+
+//! Project version string for PBStorageInterface.
+FOUNDATION_EXPORT const unsigned char PBStorageInterfaceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PBStorageInterface/PublicHeader.h>
+
+
