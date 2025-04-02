7733d7be10e734f337359fb38d73b25033136658
Siwon-L <111dltldnjs@gmail.com>
2025-04-02 14:35:00 +0900

    fix: 온보딩
    QA.1

--- Projects/App/Pobi/Onboarding/OnboardingView.swift
+++ Projects/App/Pobi/Onboarding/OnboardingView.swift
@@ -14,7 +14,7 @@ struct OnboardingView: View {
   @State private var isPresentedProfileSetting: Bool = false
   
   var body: some View {
-    VStack {
+    VStack(spacing: 0) {
       HStack {
         Spacer()
         Button {
@@ -51,7 +51,7 @@ struct OnboardingView: View {
           Text("여행, 출근, 운동 등 상황별 포켓을\n만들고 소지품을 추가하세요.")
             .font(PBFonts.body._1.font)
             .foregroundStyle(PBColors.navy._200.color)
-            .padding(.bottom, 69)
+            .padding(.bottom, 35)
           PBImages.onboardingSecond.image
         }
         .tag(1)
@@ -64,13 +64,20 @@ struct OnboardingView: View {
           Text("포비가 소지품을 챙기도록 알려주고,\n상황별 추천 리스트도 제공해요!")
             .font(PBFonts.body._1.font)
             .foregroundStyle(PBColors.navy._200.color)
-            .padding(.bottom, 69)
+            .padding(.bottom, 35)
           PBImages.onboardingThird.image
         }
         .tag(2)
       }
       .tabViewStyle(.page(indexDisplayMode: .never))
-      TabIndicator(count: 3, currentIndex: $currentIndex)
+      .overlay {
+        VStack(spacing: 0) {
+          Spacer()
+          TabIndicator(count: 3, currentIndex: $currentIndex)
+            .padding(.bottom, 62)
+        }
+      }
+
       PBRoundButton(16) {
         if currentIndex == 2 {
           isPresentedProfileSetting = true
