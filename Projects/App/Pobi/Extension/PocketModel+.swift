9430bbfe5b00083b6036f6b22ad6d67c17e746cd
Siwon-L <111dltldnjs@gmail.com>
2025-04-08 15:41:21 +0900

    chore: PBStorage에서 LocalNotiService 의존성 제거

---
+++ Projects/App/Pobi/Extension/PocketModel+.swift
@@ -0,0 +1,37 @@
+//
+//  PocketModel+.swift
+//  Pobi
+//
+//  Created by 이시원 on 4/8/25.
+//
+
+import PBStorageInterface
+import LocalNotiService
+
+extension PocketModel {
+  public func registerPushAlarm(userNickname: String) {
+    LocalNotiCenter.shared.register(
+      title: "POBI",
+      body: "똑똑! \(userNickname)님 '\(title)' 소지품 챙기세요!",
+      id: id.uuidString,
+      trigerType: pushType,
+      time: alarm.time
+    )
+  }
+  
+  public func deletePushAlarm() {
+    LocalNotiCenter.shared.remove(id: id.uuidString, type: pushType)
+  }
+  
+  private var pushType: TrigerType {
+    if repeats {
+      if alarm.isWeekRepeat {
+        return .week(weeks: alarm.days)
+      } else {
+        return .day(days: alarm.days)
+      }
+    } else {
+      return .date(alarm.date)
+    }
+  }
+}
