b1b622b2e73e12b4ce144d5c217b234809793c49
Siwon-L <111dltldnjs@gmail.com>
2025-04-11 00:22:19 +0900

    fix: 오타 수정

--- Projects/App/Pobi/Scenes/Home/Views/PocketCell.swift
+++ Projects/App/Pobi/Scenes/Home/Views/PocketCell.swift
@@ -43,7 +43,7 @@ struct PocketCell: View {
       .padding(.horizontal, 16)
       Spacer()
       HStack {
-        Text("\(pocket.items.count) itmes")
+        Text("\(pocket.items.count) items")
           .font(PBFonts.caption._2.font)
           .foregroundStyle(PBColors.navy._900.color)
         Spacer()
