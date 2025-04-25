2bdaf2edaac844cfeea23d005aad6af181a47d4e
Siwon-L <111dltldnjs@gmail.com>
2025-04-25 14:17:23 +0900

    feat-22: PocketItem

--- Projects/PBStorage/PBStorageInterface/Models/PocketItemModel.swift
+++ Projects/PBStorage/PBStorageInterface/Models/PocketItemModel.swift
@@ -46,3 +46,37 @@ extension Array where Element: PocketItemModel {
     }
   }
 }
+
+public struct PocketItem {
+  public let id: UUID
+  public let title: String
+  public let memo: String
+  public let isChecked: Bool
+  public let sortIndex: Int
+  
+  public init(
+    id: UUID = .init(),
+    title: String = "",
+    memo: String = "",
+    isChecked: Bool = false,
+    sortIndex: Int = 0
+  ) {
+    self.id = id
+    self.title = title
+    self.memo = memo
+    self.isChecked = isChecked
+    self.sortIndex = sortIndex
+  }
+  
+  public init(model: PocketItemModel) {
+    self.id = model.id
+    self.title = model.title
+    self.memo = model.memo
+    self.isChecked = model.isChecked
+    self.sortIndex = model.sortIndex
+  }
+  
+  public func toModel() -> PocketItemModel {
+    return .init(id: self.id, title: self.title, memo: self.memo, isChecked: self.isChecked, sortIndex: self.sortIndex)
+  }
+}
