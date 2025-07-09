3d81a371767fed000c823cda54af2fee34aa769e
Siwon-L <111dltldnjs@gmail.com>
2025-07-09 14:21:59 +0900

    test: MockProfileStorage

--- Projects/PBStorage/PBStorageTesting/MockProfileStorage.swift
+++ Projects/PBStorage/PBStorageTesting/MockProfileStorage.swift
@@ -8,39 +8,58 @@
 import PBStorageInterface
 
 public final class MockProfileStorage: ProfileStorable, @unchecked Sendable {
-  public var count: Int = 0
-  
-  public init() {
-    
+  public struct CallCount {
+    public var saveNotFirstEntry: Int = 0
+    public var loadNotFirstEntry: Int = 0
+    public var saveNickname: Int = 0
+    public var loadNickname: Int = 0
+    public var saveProfileImageType: Int = 0
+    public var loadProfileImageType: Int = 0
   }
   
+  public struct ReturnValue {
+    public var loadNotFirstEntry: Bool = false
+    public var loadNickname: String? = nil
+    public var loadProfileImageType: ProfileImageType? = nil
+  }
+  
+  public struct InputValue {
+    public var saveNickname: String?
+    public var saveProfileImageType: ProfileImageType?
+  }
+  
+  public var callCount = CallCount()
+  public var returnValue: ReturnValue = .init()
+  public var inputValue: InputValue = .init()
+  
+  public init() {}
+  
   public func saveNotFirstEntry() {
-    print(#function)
+    callCount.saveNotFirstEntry += 1
   }
   
   public func loadNotFirstEntry() -> Bool {
-    print(#function)
-    return true
+    callCount.loadNotFirstEntry += 1
+    return returnValue.loadNotFirstEntry
   }
   
   public func saveNickname(_ nickname: String) {
-    print(#function)
-
+    callCount.saveNickname += 1
+    inputValue.saveNickname = nickname
   }
   
   public func loadNickname() -> String? {
-    print(#function)
-    count += 1
-    return nil
+    callCount.loadNickname += 1
+    return returnValue.loadNickname
   }
   
   public func saveProfileImageType(_ type: ProfileImageType) {
-    print(#function)
-
+    callCount.saveProfileImageType += 1
+    inputValue.saveProfileImageType = type
   }
   
   public func loadProfileImageType() -> ProfileImageType? {
-    print(#function)
-    return .first
+    callCount.loadProfileImageType += 1
+    return returnValue.loadProfileImageType
   }
 }
