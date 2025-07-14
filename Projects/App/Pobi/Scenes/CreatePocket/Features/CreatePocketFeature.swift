ecace8025c78887f5c74a61250392ecf26e9bffe
Siwon-L <111dltldnjs@gmail.com>
2025-07-14 19:23:46 +0900

    feat: CreatePocketFeature create action

--- Projects/App/Pobi/Scenes/CreatePocket/Features/CreatePocketFeature.swift
+++ Projects/App/Pobi/Scenes/CreatePocket/Features/CreatePocketFeature.swift
@@ -16,6 +16,7 @@ struct CreatePocketFeature {
   struct State: Equatable {
     var pocket: Pocket = .init()
     let pocketModel: PocketModel?
+    var selectedTemplate: TemplateModel?
     
     init(pocket: PocketModel?, date: Date? = nil) {
       self.pocketModel = pocket
@@ -27,22 +28,28 @@ struct CreatePocketFeature {
   }
   
   enum Action {
-    case titleChanged(String)
-    case onAlarmChanged(Bool)
+    case setTitle(String)
+    case setOnAlarm(Bool)
+    case setTemplate(TemplateModel)
     case edit
+    case create
   }
   
   @Dependency(\.profileStorage) var profileStorage
   @Dependency(\.localNotiCenter) var localNotiCenter
   @Dependency(\.firebaseManager) var firebaseManager
+  @Dependency(\.pocketStorage) var pocketStorage
   
   var body: some ReducerOf<Self> {
     Reduce { state, action in
       switch action {
-      case .titleChanged(let title):
+      case .setTitle(let title):
         state.pocket.title = title
-      case .onAlarmChanged(let onAlarm):
+      case .setOnAlarm(let onAlarm):
         state.pocket.onAlarm = onAlarm
+      case .setTemplate(let template):
+        state.selectedTemplate = template
+        return .merge(.send(.setTitle(template.title)))
       case .edit:
         guard let pocketModel = state.pocketModel else { return .none }
         localNotiCenter.remove(id: pocketModel.id.uuidString, type: pocketModel.pushType)
@@ -60,6 +67,32 @@ struct CreatePocketFeature {
         } else {
           firebaseManager.logEvent(event: .alarmDisable)
         }
+      case .create:
+        let newPocketModel = PocketModel(state.pocket)
+        if state.pocket.onAlarm {
+          let nickName = profileStorage.loadNickname() ?? "사용자"
+          localNotiCenter.register(
+            title: "POBI",
+            body: "똑똑! \(nickName)님 '\(newPocketModel.title)' 소지품 챙기세요!",
+            id: newPocketModel.id.uuidString,
+            trigerType: newPocketModel.pushType,
+            time: newPocketModel.alarm.time
+          )
+          firebaseManager.logEvent(event: .alarmActivation)
+        } else {
+          firebaseManager.logEvent(event: .alarmDisable)
+        }
+        if state.pocket.isCalendar {
+          firebaseManager.logEvent(event: .onCalendar)
+        } else {
+          firebaseManager.logEvent(event: .offCalendar)
+        }
+        if let template = state.selectedTemplate {
+          newPocketModel.items = template.items.map { $0.copy() }
+          firebaseManager.logEvent(event: .importTemplate)
+        }
+        pocketStorage?.insert(newPocketModel)
+        firebaseManager.logEvent(event: .createPocket)
       }
       return .none
     }
--- Projects/App/PobiTests/PobiTests.swift
+++ Projects/App/PobiTests/PobiTests.swift
@@ -16,25 +16,28 @@ import ComposableArchitecture
 
 final class CreatePocketFeatureTests: XCTestCase {
   var sut: TestStore<CreatePocketFeature.State, CreatePocketFeature.Action>!
-  var mockStorage: MockProfileStorage!
+  var mockProfileStorage: MockProfileStorage!
   var mockLocalNoti: MockLocalNotiCenter!
   var mockFirebaseManager: MockFirebaseManager!
+  var mockPocketStorage: MockPocketStorage!
   
   override func setUpWithError() throws {
-    mockStorage = MockProfileStorage()
+    mockProfileStorage = MockProfileStorage()
     mockLocalNoti = MockLocalNotiCenter()
     mockFirebaseManager = MockFirebaseManager()
+    mockPocketStorage = MockPocketStorage()
     sut = TestStore(initialState: CreatePocketFeature.State(pocket: nil)) {
       CreatePocketFeature()
     } withDependencies: {
-      $0.profileStorage = mockStorage
+      $0.profileStorage = mockProfileStorage
       $0.localNotiCenter = mockLocalNoti
       $0.firebaseManager = mockFirebaseManager
+      $0.pocketStorage = mockPocketStorage
     }
   }
   
   override func tearDownWithError() throws {
-    mockStorage = nil
+    mockProfileStorage = nil
     mockLocalNoti = nil
     mockFirebaseManager = nil
     sut = nil
@@ -46,21 +49,21 @@ final class CreatePocketFeatureTests: XCTestCase {
     sut = TestStore(initialState: CreatePocketFeature.State(pocket: pocket)) {
       CreatePocketFeature()
     } withDependencies: {
-      $0.profileStorage = mockStorage
+      $0.profileStorage = mockProfileStorage
       $0.localNotiCenter = mockLocalNoti
       $0.firebaseManager = mockFirebaseManager
     }
-    mockStorage.returnValue.loadNickname = "testUser"
+    mockProfileStorage.returnValue.loadNickname = "testUser"
     
     // Act
-    await sut.send(.titleChanged("newTitle")) {
+    await sut.send(.setTitle("newTitle")) {
       $0.pocket.title = "newTitle"
     }
     await sut.send(.edit)
     // Assert
     XCTAssertEqual("newTitle", sut.state.pocketModel?.title)
     
-    XCTAssertEqual(1, mockStorage.callCount.loadNickname)
+    XCTAssertEqual(1, mockProfileStorage.callCount.loadNickname)
     
     XCTAssertEqual(1, mockLocalNoti.callCount.remove)
     XCTAssertEqual(pocket.id.uuidString, mockLocalNoti.inputValue.remove?.id)
@@ -79,26 +82,81 @@ final class CreatePocketFeatureTests: XCTestCase {
     sut = TestStore(initialState: CreatePocketFeature.State(pocket: pocket)) {
       CreatePocketFeature()
     } withDependencies: {
-      $0.profileStorage = mockStorage
+      $0.profileStorage = mockProfileStorage
       $0.localNotiCenter = mockLocalNoti
       $0.firebaseManager = mockFirebaseManager
     }
-    mockStorage.returnValue.loadNickname = "testUser"
+    mockProfileStorage.returnValue.loadNickname = "testUser"
     
     // Act
-    await sut.send(.titleChanged("newTitle")) {
+    await sut.send(.setTitle("newTitle")) {
       $0.pocket.title = "newTitle"
     }
     await sut.send(.edit)
     // Assert
     XCTAssertEqual("newTitle", sut.state.pocketModel?.title)
     
-    XCTAssertEqual(0, mockStorage.callCount.loadNickname)
+    XCTAssertEqual(0, mockProfileStorage.callCount.loadNickname)
     
     XCTAssertEqual(1, mockLocalNoti.callCount.remove)
     XCTAssertEqual(pocket.id.uuidString, mockLocalNoti.inputValue.remove?.id)
     XCTAssertEqual(pocket.pushType, mockLocalNoti.inputValue.remove?.type)
 
     XCTAssertEqual(0, mockLocalNoti.callCount.register)
+  }
+  
+  @MainActor func test_create_호출_시_onAlarm이_true이면_알림_등록_및_영구_저장소에_저장() async {
+    // Arrange
+    mockProfileStorage.returnValue.loadNickname = "testUser"
+
+    await sut.send(.setOnAlarm(true)) {
+      $0.pocket.onAlarm = true
+    }
+    await sut.send(.setTitle("Test")) {
+      $0.pocket.title = "Test"
+    }
+    
+    // Act
+    await sut.send(.create)
+    // Assert    
+    XCTAssertEqual(1, mockProfileStorage.callCount.loadNickname)
+
+    XCTAssertEqual(1, mockLocalNoti.callCount.register)
+    XCTAssertEqual("똑똑! testUser님 'Test' 소지품 챙기세요!", mockLocalNoti.inputValue.register?.body)
+    
+    XCTAssertEqual(1, mockPocketStorage.callCount.insert)
+    XCTAssertEqual("Test", mockPocketStorage.inputValue.insert?.title)
+  }
+  
+  @MainActor func test_create_호출_시_onAlarm이_false이면_알림_등록X_영구_저장소에_저장() async {
+    // Arrange
+    await sut.send(.setTitle("Test")) {
+      $0.pocket.title = "Test"
+    }
+    // Act
+    await sut.send(.create)
+    // Assert
+    XCTAssertEqual(0, mockProfileStorage.callCount.loadNickname)
+
+    XCTAssertEqual(0, mockLocalNoti.callCount.register)
+    
+    XCTAssertEqual(1, mockPocketStorage.callCount.insert)
+    XCTAssertEqual("Test", mockPocketStorage.inputValue.insert?.title)
+  }
+  
+  @MainActor func test_create_호출_시_selectedTemplate이_존재하면_() async {
+    // Arrange
+    let template = TemplateModel(title: "TestTemplate")
+    await sut.send(.setTemplate(template)) {
+      $0.selectedTemplate = template
+    }
+    await sut.receive(\.setTitle) {
+      $0.pocket.title = "TestTemplate"
+    }
+    // Act
+    await sut.send(.create)
+    // Assert
+    XCTAssertEqual(1, mockPocketStorage.callCount.insert)
+    XCTAssertEqual("TestTemplate", mockPocketStorage.inputValue.insert?.title)
   }
 }
