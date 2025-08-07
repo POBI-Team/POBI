fe9b76de3c8df9d9d54cc99f280cf358ae73b05c
Siwon-L <111dltldnjs@gmail.com>
2025-08-07 19:53:19 +0900

    chore: home 배너 삭제

--- Projects/App/Pobi/Scenes/Home/HomeView.swift
+++ Projects/App/Pobi/Scenes/Home/HomeView.swift
@@ -18,7 +18,7 @@ struct HomeView: View {
   @EnvironmentObject private var profileStorage: ProfileStorage
   @State private var selectedTabIndex: Int = 0
   @Binding private var isPresentedCreate: Bool
-  @State private var isPresentedUpdateGuide: Bool = false
+  //@State private var isPresentedUpdateGuide: Bool = false
   @State private var isAppear = false
   @State private var profileImageType: ProfileImageType = .first
   @State private var nickname: String = ""
@@ -42,40 +42,6 @@ struct HomeView: View {
             .frame(width: 48, height: 48)
         }
         
-        Button {
-          isPresentedUpdateGuide = true
-        } label: {
-          HStack {
-            VStack(alignment: .leading, spacing: 3) {
-              HStack(spacing: 4) {
-                Text("📢")
-                  .font(PBFonts.tossFace.xsmall.font)
-                Text("포비, 이렇게 바뀌었어요!")
-                  .font(PBFonts.label._1.font)
-                  .foregroundStyle(PBColors.navy._900.color)
-              }
-              Text("새롭게 추가된 기능들을 확인해보세요.")
-                .font(PBFonts.label._2.font)
-                .foregroundStyle(PBColors.navy._200.color)
-            }
-            Spacer()
-            Text("자세히 보기")
-              .font(PBFonts.button._4.font)
-              .foregroundStyle(PBColors.navy._900.color)
-              .frame(width: 75, height: 28)
-              .background(PBColors.yellow._300.color)
-              .clipShape(RoundedRectangle(cornerRadius: 8))
-            
-          }
-          .padding(.horizontal, 20)
-          .frame(height: 68)
-          .background(PBColors.yellow._10.color)
-          .clipShape(RoundedRectangle(cornerRadius: 16))
-        }
-        .padding(.top, 4)
-        .padding(.bottom, 20)
-        .buttonStyle(.plain)
-        
         PBSegmentView(
           selected: $selectedTabIndex, items: .init("내 포켓"), .init("템플릿")
         )
@@ -94,19 +60,19 @@ struct HomeView: View {
         )
       }
     }
-    .fullScreenCover(isPresented: $isPresentedUpdateGuide) {
-      PBNavigationBar {
-        WebView(url:"https://furry-gruyere-f25.notion.site/1-1-0-21c0c2771f6d80f0a7f5eb638ab0f16d?source=copy_link")
-      }
-      .title("업데이트 안내")
-      .rightItem {
-        Button {
-          isPresentedUpdateGuide = false
-        } label: {
-          PBImages.cancel.image
-        }
-      }
-    }
+//    .fullScreenCover(isPresented: $isPresentedUpdateGuide) {
+//      PBNavigationBar {
+//        WebView(url:"https://furry-gruyere-f25.notion.site/1-1-0-21c0c2771f6d80f0a7f5eb638ab0f16d?source=copy_link")
+//      }
+//      .title("업데이트 안내")
+//      .rightItem {
+//        Button {
+//          isPresentedUpdateGuide = false
+//        } label: {
+//          PBImages.cancel.image
+//        }
+//      }
+//    }
     .onAppear {
       self.profileImageType = profileStorage.loadProfileImageType() ?? .first
       self.nickname = profileStorage.loadNickname() ?? "사용자"
